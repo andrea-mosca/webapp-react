03156c82863b709c79101e7309e802b0c08799dc
andrea-mosca <andrea.moscateli@gmail.com>
2025-06-18 15:31:11 +0200

    movie list in homepage whit axios

--- src/component/MoviesListCard.jsx
+++ src/component/MoviesListCard.jsx
@@ -1,12 +1,24 @@
 import MovieCard from "./MovieCard";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function MovieListCard() {
+  const [movies, setMovies] = useState([]);
+  useEffect(() => {
+    axios
+      .get("http://localhost:3000/movies")
+      .then((res) => setMovies(res.data.movies));
+  }, []);
+  console.log(movies);
   return (
     <div className="container pt-5">
       <div className="row row-cols-2">
-        <div className="col">
-          <MovieCard />
-        </div>
+        {movies &&
+          movies.map((movie) => (
+            <div className="col" key={movie.id}>
+              <MovieCard {...movie} />
+            </div>
+          ))}
       </div>
     </div>
   );
